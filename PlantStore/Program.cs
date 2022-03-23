b9c4c762179a5cbd0579556344a9228185a97c8e
Gianny <Gianny281@revature.net>
2022-03-23 18:03:45 -0400

    added greeting

--- PlantStore/Program.cs
+++ PlantStore/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to PlantStore!");
+Console.WriteLine("Have you shopped with us before?");
